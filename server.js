4d4d286ae23f8a1200b8477d772f55e1926a4bb2
kiran364 <kumawatkiran364@gmail.com>
2021-10-16 10:40:20 +0530

    updated

--- server.js
+++ server.js
@@ -17,8 +17,93 @@ const {Telegraf} = require('telegraf')
 
 const bot = new Telegraf(process.env.botId)
 
-bot.use((ctx) => {
-    ctx.reply('hiiii human!!!')
+
+bot.help((ctx) => {
+    ctx.reply("This Bot Perform Following Commands\n - /start\n - /help");
+})
+
+bot.start((ctx) => {
+    let name = ctx.from.first_name
+    ctx.reply(`Hi, ${name} Where are you from?`)}
+)
+
+bot.command('restart', (ctx) => {
+    let name = ctx.from.first_name
+    ctx.reply(`Hi, ${name} Where are you from?`)}
+)
+
+
+bot.on('text', (ctx) => {
+    ctx.reply(`How many vaccines are taken?`,{
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    { text: 'one', callback_data: 'one' },
+                ],
+                [
+                    { text: 'two', callback_data: 'two' },
+                ]
+            ]
+        }
+    })
+})
+
+// 
+
+
+bot.action('one', ctx => {
+    console.log(ctx.from)
+    let response = `Do you have any symptons?`;
+    ctx.deleteMessage();
+    ctx.reply(response, {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    { text: "Yes", callback_data: 'yes' },
+                ],
+                [
+                    { text: "No", callback_data: 'no' },
+                ],
+            ]
+        }
+    })
+})
+
+
+bot.action('yes', ctx => {
+    console.log(ctx.from)
+    let response = `Do you have any symptons?`;
+    ctx.deleteMessage();
+    ctx.reply(response, {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    { text: "cough", callback_data: 'cough' },
+                ],
+                [
+                    { text: "fever", callback_data: 'fever' },
+                ],
+            ]
+        }
+    })
+})
+
+bot.action('fever', ctx => {
+    let response = `Take a cosfils if cough increases contact to doctor`;
+    ctx.deleteMessage();
+    ctx.reply(response)
+})
+
+bot.action('cough', ctx => {
+    let response = `Take a cosfils if cough increases contact to doctor`;
+    ctx.deleteMessage();
+    ctx.reply(response)
+})
+
+bot.action('two', ctx => {
+    let response = `Congrats you are full vaccinated! but fight is not over yet `;
+    ctx.deleteMessage();
+    ctx.reply(response)
 })
 
 bot.launch()
